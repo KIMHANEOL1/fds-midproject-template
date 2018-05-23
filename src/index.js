9bce65dbcc46402861ae920c002f24cefd4367f3
Kim HanEol <himnemesis@gmail.com>
2018-05-23 15:52:47 +0900

    로그인  로그아웃 버튼숨기기

--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
 import axios from 'axios';
 
+const postAPI = axios.create({});
+const rootEl = document.querySelector('.root');
+
+if(localStorage.getItem('token')) {
+  postAPI.dafaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+  rootEl.classList.add('root--authed');
+}
 
 
-axios.get('https://localhost:3000/posts').then(res=>{
+const templates = {
+  postList: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content,
+  postContent: document.querySelector('#post-content').content,
+  login: document.querySelector('#login').content,
+}
+
+
+function render(fragment){
+  rootEl.textContent='';
+  rootEl.appendChild(fragment);
+}
+
+async function indexPage() {
+    const res = await postAPI.get('http://localhost:3000/posts');
+    const listFragment = document.importNode(templates.postList, true);  
+    
+    listFragment.querySelector('.post-list__login-btn').addEventListener('click', e=>{
+      loginPage();
+    })
+
+    listFragment.querySelector('.post-list__logout-btn').addEventListener('click', e=>{
+      localStorage.removeItem('token');
+      delete postAPI.defaults.headers['Authorization'];
+      rootEl.classList.remove('root--authed');
+      indexPage();
+    })
+
     res.data.forEach(post => {
-      const fragment = document.importNode(document.querySelector('#post-item').content, true);
+      const fragment = document.importNode(templates.postItem,true);
       const pEl= fragment.querySelector('.post-item__title'); 
       pEl.textContent = post.title;
-      document.querySelector('.post-list').appendChild(fragment);
-    })
-})
+      pEl.addEventListener('click', e=>{
+          postContentPage(post.id);
+      })
+      listFragment.querySelector('.post-list').appendChild(fragment);
+    });
+    render(listFragment);
+}
+
+async function postContentPage(postId){
+   const res = await postAPI.get(`http://localhost:3000/posts/${postId}`); 
+   const fragment = document.importNode(templates.postContent, true);
+   fragment.querySelector('.post-content__title').textContent = res.data.title;
+   fragment.querySelector('.post-content__body').textContent = res.data.body;
+   fragment.querySelector('.post-content__back-btn').addEventListener('click', e=>{
+        indexPage();
+   })
+  
+    render(fragment);
+  }
+
+async function loginPage(){
+  const fragment= document.importNode(templates.login, true);
+  const forEl= fragment.querySelector('.login__form');
+  forEl.addEventListener('submit', async e => {
+      
+    //e.target.elements.username===fragment.querySelector('.login__username');
+    const payload = {
+         username: e.target.elements.username.value,
+         password: e.target.elements.password.value
+      };
+      e.preventDefault();
+      const res = await postAPI.post('http://localhost:3000/users/login', payload);
+      localStorage.setItem('token', res.data.token);
+      postAPI.defaults.headers['Authorization'] = `Bearer ${res.data.token}`;
+      rootEl.classList.add('.root');~~
+      indexPage();
+  })
+  render(fragment);
+}
+indexPage();
+
+
+
